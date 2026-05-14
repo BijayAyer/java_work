a817bd050dc5bb816a4c8c131610eca831d60d56
BijayAyer <bijayayer68@gmail.com>
2026-05-14 10:13:27 +0545

    First commit

---
+++ src/DataType.java
@@ -0,0 +1,19 @@
+public class DataType {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b  = 10.1;
+        boolean isthis = true;
+        char c = 'A';
+        String d = "Bijay";
+
+        System.out.println("integer = "  +a);
+        System.out.println("double = "  +b);
+        System.out.println("char = "  +c);
+        System.out.println("String = "  +d);
+        System.out.println("Boolean = "  +isthis);
+
+    }
+
+}
