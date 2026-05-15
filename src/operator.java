48c7973a334e90156de1eb496a8cd3e1fe9821c9
BijayAyer <bijayayer68@gmail.com>
2026-05-15 20:05:00 +0545

    Operator

---
+++ src/operator.java
@@ -0,0 +1,22 @@
+public class operator {
+
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 5;
+        int add , sub , div , mul;
+
+        add = a + b;
+        sub = a - b;
+        div = a / b;
+        mul = a * b;
+
+        System.out.println("Sum = "  +add);
+        System.out.println("Subtraction = "  +sub);
+        System.out.println("Division = "  +div);
+        System.out.println("Multiplication = "  +mul);
+
+
+
+    }
+}
