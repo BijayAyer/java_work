2ac7ba568015e9433c33af2117b55d0941576880
BijayAyer <bijayayer68@gmail.com>
2026-05-15 20:13:19 +0545

    Logical operator

--- src/operator.java
+++ src/operator.java
@@ -4,6 +4,7 @@ public class operator {
 
         int a = 10;
         int b = 5;
+        int c = 4;
         int add , sub , div , mul;
     //arithmatic operators
         add = a + b;
@@ -11,18 +12,31 @@ public class operator {
         div = a / b;
         mul = a * b;
 
+
+        System.out.println("Arithmetic operator");
         System.out.println("Sum = "  +add);
         System.out.println("Subtraction = "  +sub);
         System.out.println("Division = "  +div);
         System.out.println("Multiplication = "  +mul);
 
     // Relational Operators
-
+        System.out.println("----------------------------");
+        System.out.println("Relational Operator");
         System.out.println(a > b);
         System.out.println(a < b);
         System.out.println(a >= b);
         System.out.println(a <= b);
         System.out.println(a == b);
+
+    //Logical Operator
+        System.out.println("----------------------------");
+        System.out.println("Logical operator");
+        System.out.println(a > b &&  a > c);
+        System.out.println(a > b || a < c);
+        System.out.println(!(a > b));
+
+
+        
 
 
     }
