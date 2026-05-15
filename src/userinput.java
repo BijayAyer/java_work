357937b5baa1decab0e7d9a85d00161754165180
BijayAyer <bijayayer68@gmail.com>
2026-05-15 20:27:17 +0545

    new image 2

--- src/userinput.java
+++ src/userinput.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class userinput {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, sum;
+
+        // Taking input from user
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        b = sc.nextInt();
+
+        // Adding numbers
+        sum = a + b;
+
+        // Display result
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
 }
